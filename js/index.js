149e0715c9e7139aec69785e25dac7e9c2d6dd5e
vaskae <titu200@gmail.com>
2018-12-03 08:43:29 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 300) {
+	    $(".black").css("background" , "blue");
+	  }
+
+	  else{
+		  $(".black").css("background" , "#333");
+ 
+
+	
+  }
+ 
+
+
+var scroll = $(window).scrollTop();
+	  if (scroll > 300) {
+	    $(".logo").css("display" , "block");	  }
+
+	  else{
+
+		 $(".logo").css("display" , "none");
+ 
+
+	
+  }
+ 
+
+ })
+})
+
+
+$(".logo").css("display" , "none");   
+ $(".logo").css("display" , "none");  
